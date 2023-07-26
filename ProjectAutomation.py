845293c8ce8b65f54e9c225ccb175fce85e061df
Rishabh SONI <124634185+Pario398@users.noreply.github.com>
2023-07-26 19:45:19 +0100

    Create ProjectAutomation.py

---
+++ ProjectAutomation.py
@@ -0,0 +1,18 @@
+import os
+
+def ProjAutomation():
+    destinationPath = input("Enter the directory path to create the project folder: ")
+    projName = input("Enter the project name: ")
+    projPath = os.path.join(destinationPath, projName)
+    try:
+        os.makedirs(projPath)
+        print(f"Project folder '{projName}' created successfully.")
+    except FileExistsError:
+        print(f"Project folder '{projName}' already exists.")
+    readmePath = os.path.join(projPath, "README.md")
+    with open(readmePath, "w") as readmeFile:
+        readmeFile.write(f"# {projName}\n")
+        readmeFile.write(f"\nEnter Contents into this ReadMe file for documentation\n")
+    print("README.md file created successfully.")
+
+ProjAutomation()
